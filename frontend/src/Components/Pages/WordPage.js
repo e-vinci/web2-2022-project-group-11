3d21c76e8628b243361b49fe6303c240d32c0923
Senhmoh <mohammed.senhaji@student.vinci.be>
2022-12-16 15:15:28 +0100

    redesign tableau mots admin

--- frontend/src/Components/Pages/WordPage.js
+++ frontend/src/Components/Pages/WordPage.js
@@ -1,6 +1,57 @@
+const isAdmin = false;
 const WordPage = () => {
     const main = document.querySelector('main');
-    const admin = document.querySelector('admin');
+    if (!isAdmin){
+    main.innerHTML = 
+        ` 
+        <p class="ranking">Vous devez etre connecté pour ajouter un mot</p>
+        <div class="wrapper-box">
+            <div class="curved" id="curved"></div>
+            <div class="wrapper">
+            <h1>Suggerez un mot et son synonyme</h1>
+                <div class="input-box"> 
+                    <input type="text" name="word" placeholder="Mot" id= "word">
+                </div>
+              
+                <div class="input-box"> 
+                    <input  type="text" name="synonym" placeholder="Synonyme" id="synonym">
+                </div>
+                <div>
+                <button id="word_btn" type="submit">Soumettre</button>
+                </div>
+            </form>
+            </div>
+            </div>
+        <div class="">
+        <h1>Mots et synonymes soumis (admin)</h1>
+        <table>
+        <thead>
+            <tr>
+                <th>Mot</th>
+                <th>Synonyme</th>
+            </tr>
+        </thead>
+        <tbody>
+            <form action="/members/admin/question" method="post">
+                <tr>
+                    <td>exemple</td>
+                    <td>exemple</td>
+                    <input type="hidden" name="word_id" value=>
+                    <td>
+                        <input type="submit" value="Accepter" name="btn" class="accepter">
+                    </td>
+                    <td>
+                        <input type="submit" value="Refus" name="btn" class="refus">
+                    </td>
+                </tr>
+                </form>
+        </tbody>
+    </table>
+    </div>
+    `;
+
+   }
+   else{
     main.innerHTML = `
     <p class="ranking">Vous devez etre connecté pour ajouter un mot</p>
     
@@ -23,30 +74,7 @@ const WordPage = () => {
         </div>
 
     `;
-
-    //if (isAdmin){
-    admin.innerHTML = 
-        ` 
-        <h1>Mots et synonymes soumis</h1>
-        <table>
-        <thead>
-            <tr>
-                <th>Mot</th>
-                <th>Sujet</th>  
-            </tr>
-        </thead>
-        <tbody>
-        
-                <tr>
-                    <td>exemple mot </td>
-                    <td>exemple synonyme </td>
-                </tr>
-        
-        </tbody>
-    </table>
-    `;
-
-  //  }
+   }
 
   };
   
