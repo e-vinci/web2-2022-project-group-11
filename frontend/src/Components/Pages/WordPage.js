cb00925d7c687cb04199853a2554e6fa840dfca9
Senhmoh <mohammed.senhaji@student.vinci.be>
2022-12-16 13:00:09 +0100

    modif formulaire Wordpage

--- frontend/src/Components/Pages/WordPage.js
+++ frontend/src/Components/Pages/WordPage.js
@@ -3,6 +3,24 @@ const WordPage = () => {
     main.innerHTML = `
     <p class="ranking">Vous devez etre connecté pour ajouter un mot</p>
     
+    <div class="wrapper-box">
+        <div class="curved" id="curved"></div>
+        <div class="wrapper">
+        <h1>Suggerez un mot et son synonyme</h1>
+            <div class="input-box"> 
+                <input type="text" name="word" placeholder="Mot" id= "word">
+            </div>
+          
+            <div class="input-box"> 
+                <input  type="text" name="synonym" placeholder="Synonyme" id="synonym">
+            </div>
+            <div>
+            <button id="word_btn" type="submit">Soumettre</button>
+            </div>
+        </form>
+        </div>
+        </div>
+
     `;
   };
   
